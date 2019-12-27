b99eb7a7d3cabb0f1b48a6bfbdb709b2b24c5d71
John Cadena <cadena.john.85@gmail.com>
2019-12-27 08:40:57 -0800

    refactor HumanReadableTime to use String.format more efficiently

--- src/HumanReadableTime.java
+++ src/HumanReadableTime.java
@@ -1,14 +1,22 @@
 public class HumanReadableTime {
-    public static String makeReadable(int intervalInSeconds) {
-        int hours = intervalInSeconds / 3600;
-        int minutes = (intervalInSeconds / 60) % 60;
-        int seconds = intervalInSeconds % 60;
-        return String.format("%s:%s:%s", pad(hours), pad(minutes), pad(seconds));
+
+    //Original solution
+
+//    public static String makeReadable(int intervalInSeconds) {
+//        int hours = intervalInSeconds / 3600;
+//        int minutes = (intervalInSeconds / 60) % 60;
+//        int seconds = intervalInSeconds % 60;
+//        return String.format("%s:%s:%s", pad(hours), pad(minutes), pad(seconds));
+//    }
+//
+//    public static String pad(int value) {
+//        return value < 10 ? "0" + value : Integer.toString(value);
+//    }
+
+    public static String makeReadable(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
     }
 
-    public static String pad(int value) {
-        return value < 10 ? "0" + value : Integer.toString(value);
-    }
 
     public static void main(String[] args) {
         System.out.println(makeReadable(1938));
