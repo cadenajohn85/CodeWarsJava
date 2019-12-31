4408f65dc6d4b21d3390705c5f3e7fd09b0d8468
John Cadena <cadena.john.85@gmail.com>
2019-12-31 09:55:33 -0600

    refactor GreedIsGood based on community solutions

--- src/GreedIsGood.java
+++ src/GreedIsGood.java
@@ -12,28 +12,38 @@
 //(contributing to the 500 points) or as a single 50 points, but not both in the same roll.
 
 public class GreedIsGood {
-    public static int greedy(int[] dice){
-        int[] counts = new int[6];
-        for (int i = 0; i < dice.length; i++) {
-            for (int j = 1; j <= 6; j++) {
-                if (dice[i] == j) {
-                    counts[j-1] += 1;
-                }
-            }
-        }
-        int score = 0;
-        if (counts[0] >= 3) {
-            score += 1000;
-            counts[0] -= 3;
-        }
-        for (int k = 1; k < counts.length; k++) {
-            if (counts[k] >= 3) {
-                score += (k + 1) * 100;
-                counts[k] -= 3;
-            }
-        }
-        score += counts[0] * 100 + counts[4] * 50;
-        return score;
+    // Original Solution
+//    public static int greedy(int[] dice){
+//        int[] counts = new int[6];
+//        for (int i = 0; i < dice.length; i++) {
+//            for (int j = 1; j <= 6; j++) {
+//                if (dice[i] == j) {
+//                    counts[j-1] += 1;
+//                }
+//            }
+//        }
+//        int score = 0;
+//        if (counts[0] >= 3) {
+//            score += 1000;
+//            counts[0] -= 3;
+//        }
+//        for (int k = 1; k < counts.length; k++) {
+//            if (counts[k] >= 3) {
+//                score += (k + 1) * 100;
+//                counts[k] -= 3;
+//            }
+//        }
+//        score += counts[0] * 100 + counts[4] * 50;
+//        return score;
+//    }
+
+    // Refactor based on community submissions
+
+    public static int greedy(int[] dice) {
+        int counts[] = new int[7];
+        for (int die : dice) counts[die]++;
+        return counts[1]/3*1000 + counts[1]%3*100 + counts[2]/3*200 + counts[3]/3*300 + counts[4]/3*400 +
+                counts[5]/3*500 + counts[5]%3*50 + counts[6]/3*600;
     }
 
     public static void main(String[] args) {
